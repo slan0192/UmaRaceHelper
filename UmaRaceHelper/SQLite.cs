ac03941dca9f7fdcf36d01d1e3fc2a60d3498ad5
tom <poiuytrewq0229@hotmail.com>
2021-05-30 17:55:50 +0900

    refactor sqlite util

--- UmaRaceHelper/SQLite.cs
+++ UmaRaceHelper/SQLite.cs
@@ -10,11 +10,6 @@ namespace UmaRaceHelper
         static extern int SHGetKnownFolderPath([MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags, IntPtr hToken, out IntPtr pszPath);
 
         private static string mDbPath = "";
-        private enum ReturnType {
-            INT,
-            DOUBLE,
-            TEXT,
-        };
 
         /* race info:
          * 育成モードの時 single_mode_program テーブルの race_instance_id から取得（id=program_id）
@@ -22,72 +17,83 @@ namespace UmaRaceHelper
          * race table の course_set から race_cource_set table の ID が取れる(id=race_id)
          * race_course_set table の distance から距離が取れる。
          */
+
         public static string getUmamusuName(int id)
         {
-            string cmd = "select * from text_data where category=170 and \"index\"=" + id.ToString();
-            return getData(cmd, "text", ReturnType.TEXT);
+            using (var cn = open())
+            {
+                return getDataFromTextDataTable(cn, 170, id);
+            }
         }
 
         public static string getMobName(int id)
         {
-            string cmd = "select * from text_data where category=59 and \"index\"=" + id.ToString();
-            return getData(cmd, "text", ReturnType.TEXT);
+            using (var cn = open())
+            {
+                return getDataFromTextDataTable(cn, 59, id);
+            }
         }
 
         public static string getRaceName(int id)
         {
-            string cmd = "select * from text_data where category=29 and \"index\"=" + id.ToString();
-            return getData(cmd, "text", ReturnType.TEXT);
+            using (var cn = open())
+            {
+                return getDataFromTextDataTable(cn, 29, id);
+            }
         }
 
         public static string getRaceNameFromProgramId(int id)
         {
             string cmd = "select * from single_mode_program where id=" + id.ToString();
-            string race_id = getData(cmd, "race_instance_id", ReturnType.INT);
-            cmd = "select * from text_data where category=29 and \"index\"=" + race_id;
-            return getData(cmd, "text", ReturnType.TEXT);
+            using (var cn = open())
+            {
+                int raceId = Convert.ToInt32(getData(cn, cmd, "race_instance_id"));
+                return getDataFromTextDataTable(cn, 29, raceId);
+            }
         }
 
         public static string getSkillName(int id)
         {
-            string cmd = "select * from text_data where category=47 and \"index\"=" + id.ToString();
-            return getData(cmd, "text", ReturnType.TEXT);
+            using (var cn = open())
+            {
+                return getDataFromTextDataTable(cn, 47, id);
+            }
         }
 
-        private static string getData(string cmdString, string key, ReturnType type)
+        private static SQLiteConnection open()
         {
             if (mDbPath == "")
                 getDBPath();
 
             var sqlConnectionSb = new SQLiteConnectionStringBuilder { DataSource = mDbPath };
-            using (var cn = new SQLiteConnection(sqlConnectionSb.ToString()))
-            {
-                cn.Open();
+            var cn = new SQLiteConnection(sqlConnectionSb.ToString());
+            cn.Open();
 
-                using (var cmd = new SQLiteCommand(cn))
+            return cn;
+        }
+
+        private static string getDataFromTextDataTable(SQLiteConnection cn,
+            int category, int index)
+        {
+            string cmd = "select * from text_data where category=" + category.ToString() + " and \"index\"=" + index.ToString();
+            return getData(cn, cmd, "text");
+        }
+
+        private static string getData(SQLiteConnection cn,
+            string cmdString, string key)
+        {
+            using (var cmd = new SQLiteCommand(cn))
+            {
+                cmd.CommandText = cmdString;
+                using (var reader = cmd.ExecuteReader())
                 {
-                    cmd.CommandText = cmdString;
-                    using (var reader = cmd.ExecuteReader())
+                    if (reader.Read())
                     {
-                        if (reader.Read())
-                        {
-                            switch (type)
-                            {
-                                case ReturnType.INT:
-                                    return reader[key].ToString();
-                                case ReturnType.DOUBLE:
-                                    return reader[key].ToString();
-                                case ReturnType.TEXT:
-                                    return reader[key].ToString();
-                                default:
-                                    return "Unknown";
-                            }
-                        }
+                        return reader[key].ToString();
                     }
                 }
             }
-
-            return "Unknown";
+            return "";
         }
 
         private static void getDBPath()
